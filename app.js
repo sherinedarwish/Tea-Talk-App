01ef13f45b1f98f69492b4eba734ac582fcb3ce8
Sherine Darwish <sheriinekhaled97@gmail.com>
2021-07-22 07:17:48 +0200

    starting

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on ${PORT}`));
+
+module.exports = app;
