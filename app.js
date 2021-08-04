d8570b89f846e9cc3b8c76a732dfb671c256acaf
Sherine Darwish <sheriinekhaled97@gmail.com>
2021-08-04 22:40:29 +0200

    heroku deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 //const redisClient = require('./helpers/init_redis');
 var createError = require('http-errors');
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -11,12 +11,13 @@ const passport= require("passport");
 const flash = require("connect-flash");
 const session = require("express-session");
 const methodOverride = require("method-override");
-const redisStore = require('connect-redis')(session);   
+//const redisStore = require('connect-redis')(session);   
 var cloudinary = require('cloudinary').v2;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(express.json());
 
 //Passport config
 require("./config/passport")(passport);
@@ -28,11 +29,19 @@ app.set("layout", "./layout");
 
 // Body Parser
 app.use(logger("dev"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// Express session
+app.use(
+  session({
+      secret: "secret",
+      resave: true,
+      saveUninitialized: true,
+  })
+);
 
 // MongoDB
 mongoose
@@ -48,15 +57,6 @@ cloudinary.config({
   api_key: process.env.API_KEY, 
   api_secret:  process.env.API_SECRET 
 });
-
-// Express session
-app.use(
-  session({
-      secret: "secret",
-      resave: true,
-      saveUninitialized: true,
-  })
-);
 
 
 // Method Override
@@ -79,17 +79,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 // App Router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -101,7 +90,8 @@ app.use('/users', usersRouter);
 // });
 
 
+module.exports = app;
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server running on ${PORT}`));
-
-module.exports = app;
